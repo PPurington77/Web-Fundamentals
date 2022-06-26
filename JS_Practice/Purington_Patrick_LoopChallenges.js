8a5ea40a46645493188b544b328b6e9f6c7aaf64
PPurington77 <papurington@gmail.com>
2022-06-26 10:19:32 -0600

    completed loop challenges

---
+++ JS_Practice/Purington_Patrick_LoopChallenges.js
@@ -0,0 +1,39 @@
+// //1. Print odds 1-20 
+// for(let i = 1; i <=20; i++) {
+//     if(i % 2 != 0) {
+//         console.log(i);
+//     }
+// }
+// //works as it should
+
+// //2. Decreasing multiples of 3
+// for(let i = 100; i >= 0; i--) {
+//     if(i % 3 == 0) {
+//         console.log(i);
+//     }
+// }
+// //works as it should
+
+// //3. Print the sequense
+// var arr = [4,2.5,1,-.5,-2,-3.5]
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+// //works as it should
+
+//4. Sigma
+// var sum = 0;
+// for(let i = 1; i <= 100; i++) {
+//     sum += i
+// }
+// console.log(sum);
+
+// //works as it should
+
+//5. Factorial
+// var product = 1
+// for(let i = 1; i <= 12; i++) {
+//     product *= i;
+// }
+// console.log(product);
+// //works as it should
