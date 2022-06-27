abfab16501501efc2813bd3a5ecc3efaacd1cd80
PPurington77 <papurington@gmail.com>
2022-06-26 18:46:50 -0600

    completed video assignment

--- JS_Practice/Button_Clicker/script.js
+++ JS_Practice/Button_Clicker/script.js
@@ -1,18 +1,19 @@
-// function loginButton(element) {
-//     element.innerText = "Log Out" 
-// }
+function loginButton(element) {
+    element.innerText = "Log Out" 
+}
+
 
 function hide(element) {
     element.remove();
 }
 
-function loginButton(element) {
-    if(element.innerText = "Log in") {
-        element.innerText = "Log Out" //this part still works as it did above
-    }
-    else if(element.innerText = "Log Out") { //this part doesn't work?
-        element.innerText = "Log in"
-    }
-}
+// function loginButton(element) {
+//     if(element.innerText = "Log in") {
+//         element.innerText = "Log Out" //this part still works as it did above
+//     }
+//     else if(element.innerText = "Log Out") { //this part doesn't work?
+//         element.innerText = "Log in"
+//     }
+// }
 
 //just trying the above
